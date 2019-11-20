7fb2a31ad818911eaca9ac6272e8457cd015da3c
jonathanyeh5566 <57975108+jonathanyeh5566@users.noreply.github.com>
2019-11-20 11:33:45 +0800

    Update 質數.py

--- 質數.py
+++ 質數.py
@@ -5,19 +5,16 @@
 
 
 a = int(input("請輸入 a 的值"))
-c=1
-if a==2:
-    print("是")
-maxno = a
+c=0
+print("因數:",end=" ")
 if a<0:
     print("不是")
 else:
     for i in range(1, a+1):
         if (a%i)==0:
-            print(int(i))
+            print(int(i),end=" ")
             c+=1
 if c==2:
     print("是")
 else:
     print("不是")
-
