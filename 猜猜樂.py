cd3284936529f44651f76dc892aad7be8ec30ad9
jonathanyeh5566 <57975108+jonathanyeh5566@users.noreply.github.com>
2019-12-03 08:22:23 +0800

    Update 猜猜樂.py

--- 猜猜樂.py
+++ 猜猜樂.py
@@ -1,12 +1,14 @@
 n=87
-g=int(input("請輸入1-99內的常數:"))
-if g <=0:
-    print("錯誤")
-elif g >=100:
-    print("錯誤")
-elif g>n:
-    print("小一點")
-elif g<n:
-    print("大一點")
-else:
-    print("bingo")
+g=0
+while (g!=n):
+    g=int(input("請輸入1-99內的常數:"))
+    if g <=0:
+        print("錯誤")
+    elif g >=100:
+        print("錯誤")
+    elif g>n:
+        print("小一點")
+    elif g<n:
+        print("大一點")
+    else:
+        print("bingo")
