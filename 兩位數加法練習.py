118222dd12b24976d962a41f7b393c86546a5c3a
jonathanyeh5566 <57975108+jonathanyeh5566@users.noreply.github.com>
2019-12-11 11:29:01 +0800

    Update 兩位數加法練習.py

--- 兩位數加法練習.py
+++ 兩位數加法練習.py
@@ -21,14 +21,13 @@ print("成功")
 
 #
 c=0
-w=0
-while w<5 :
+
+for z in range(1,6):
     import random
     a=random.randint(10,99)
     b=random.randint(10,99)
     q=a+b
     n=int(input("請輸入%d+%d=?"%(a,b)))
-    w+=1
     if q==n:
         print("對")
         c=c+1
@@ -36,4 +35,4 @@ while w<5 :
     else:
         print("錯")
         continue
-print(w)
+print(c)
