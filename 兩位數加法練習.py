d1c21a21b30caad8e6edb6f9b5180e25c89f1a83
jonathanyeh5566 <57975108+jonathanyeh5566@users.noreply.github.com>
2019-12-11 11:25:24 +0800

    Update and rename 兩位數加法練習 to 兩位數加法練習.py

--- 兩位數加法練習.py
+++ 兩位數加法練習.py
@@ -15,3 +15,25 @@ while c<3 :
         c=0
         continue
 print("成功")
+
+
+
+
+#
+c=0
+w=0
+while w<5 :
+    import random
+    a=random.randint(10,99)
+    b=random.randint(10,99)
+    q=a+b
+    n=int(input("請輸入%d+%d=?"%(a,b)))
+    w+=1
+    if q==n:
+        print("對")
+        c=c+1
+        continue
+    else:
+        print("錯")
+        continue
+print(w)
