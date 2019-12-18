a390a41b616e46d5e72ae66b389631f2e82dfc49
jonathanyeh5566 <57975108+jonathanyeh5566@users.noreply.github.com>
2019-12-18 10:51:13 +0800

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+n1="李均菱"
+n2="夏芳如"
+n3="連英亨"
+n4="吳靜怡"
+n5="李敬賢"
+chi1=87
+chi2=66
+chi3=55
+chi4=48
+chi5=92
+eng1=92
+eng2=55
+eng3=89
+eng4=10
+eng5=5
+math1=23
+math2=84
+math3=45
+math4=54
+math5=27
+print(n1+"的總分為:"+str(chi1+eng1+math1))
+print(n2+"的總分為:"+str(chi2+eng2+math2))
+print(n3+"的總分為:"+str(chi3+eng3+math3))
+print(n4+"的總分為:"+str(chi4+eng4+math4))
+print(n5+"的總分為:"+str(chi5+eng5+math5))
+
